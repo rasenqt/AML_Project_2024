2090516f88a5b402f695e2766c782ccd59c5b466
rasenqt <s310178@studenti.polito.it>
2024-02-04 17:18:50 +0100

    add point_cloud_noralizer

---
+++ 3DHighlighter-main/Normalization/PointCloudNormalizer.py
@@ -0,0 +1,13 @@
+import copy
+from . import Normalizer
+
+
+class MeshNormalizer:
+    def __init__(self, point_cloud):
+        self.point_cloud = point_cloud  # original copy of the mesh
+        self.normalizer = Normalizer.get_bounding_sphere_normalizer(self.point_cloud.vertices)
+
+    def __call__(self):
+        self.point_cloud.vertices = self.normalizer(self.point_cloud.vertices)
+        return self.point_cloud
+
